cabe62550c15208c2e9b288b6212c613383a600a
Jay Aisenbrey <jaisenbrey@broadleafcommerce.com>
2016-07-14 15:52:32 -0500

    BroadleafCommerce/QA#2125 - Add support for the common presentation Thymeleaf compatibility layer

--- src/main/java/org/broadleafcommerce/vendor/sample/web/processor/SamplePaymentGatewayHostedActionProcessor.java
+++ src/main/java/org/broadleafcommerce/vendor/sample/web/processor/SamplePaymentGatewayHostedActionProcessor.java
@@ -17,20 +17,22 @@
  * limitations under the License.
  * #L%
  */
+
 package org.broadleafcommerce.vendor.sample.web.processor;
 
 import org.broadleafcommerce.common.payment.dto.PaymentRequestDTO;
 import org.broadleafcommerce.common.payment.dto.PaymentResponseDTO;
 import org.broadleafcommerce.common.payment.service.PaymentGatewayHostedService;
 import org.broadleafcommerce.common.vendor.service.exception.PaymentException;
+import org.broadleafcommerce.common.web.dialect.AbstractBroadleafAttributeModifierProcessor;
+import org.broadleafcommerce.common.web.domain.BroadleafAttributeModifier;
+import org.broadleafcommerce.common.web.domain.BroadleafTemplateContext;
 import org.broadleafcommerce.vendor.sample.service.payment.SamplePaymentGatewayConstants;
 import org.springframework.stereotype.Component;
-import org.thymeleaf.Arguments;
-import org.thymeleaf.dom.Element;
-import org.thymeleaf.processor.attr.AbstractAttributeModifierAttrProcessor;
-import org.thymeleaf.standard.expression.StandardExpressionProcessor;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
@@ -53,35 +55,31 @@ import javax.annotation.Resource;
  * @author Elbert Bautista (elbertbautista)
  */
 @Component("blSamplePaymentGatewayHostedActionProcessor")
-public class SamplePaymentGatewayHostedActionProcessor extends AbstractAttributeModifierAttrProcessor {
+public class SamplePaymentGatewayHostedActionProcessor extends AbstractBroadleafAttributeModifierProcessor {
 
     @Resource(name = "blSamplePaymentGatewayHostedService")
     private PaymentGatewayHostedService paymentGatewayHostedService;
 
-    /**
-     * Sets the name of this processor to be used in Thymeleaf template
-     */
-    public SamplePaymentGatewayHostedActionProcessor() {
-        super("sample_payment_hosted_action");
+    @Override
+    public String getName() {
+        return "sample_payment_hosted_action-size-link";
     }
-
+    
     @Override
     public int getPrecedence() {
         return 10000;
     }
-
+    
     @Override
-    protected Map<String, String> getModifiedAttributeValues(Arguments arguments, Element element, String attributeName) {
-        Map<String, String> attrs = new HashMap<String, String>();
-
-        PaymentRequestDTO requestDTO = (PaymentRequestDTO) StandardExpressionProcessor.processExpression(arguments, element.getAttributeValue(attributeName));
+    public BroadleafAttributeModifier getModifiedAttributes(String tagName, Map<String, String> tagAttributes, String attributeName, String attributeValue, BroadleafTemplateContext context) {
+        PaymentRequestDTO requestDTO = (PaymentRequestDTO) context.parseExpression(attributeValue);
         String url = "";
-
+        Map<String, String> newAttributes = new HashMap<>();
+        List<String> removedAttributes = new ArrayList<>();
         if (requestDTO != null) {
-            if ( element.getAttributeValue("complete_checkout") != null) {
-                Boolean completeCheckout = (Boolean) StandardExpressionProcessor.processExpression(arguments,
-                        element.getAttributeValue("complete_checkout"));
-                element.removeAttribute("complete_checkout");
+            if (tagAttributes.get("complete_checkout") != null) {
+                Boolean completeCheckout = (Boolean) context.parseExpression(tagAttributes.get("complete_checkout"));
+                removedAttributes.add("complete_checkout");
                 requestDTO.completeCheckoutOnCallback(completeCheckout);
             }
 
@@ -92,23 +90,7 @@ public class SamplePaymentGatewayHostedActionProcessor extends AbstractAttribute
                 throw new RuntimeException("Unable to Create Sample Payment Gateway Hosted Link", e);
             }
         }
-
-        attrs.put("action", url);
-        return attrs;
-    }
-
-    @Override
-    protected ModificationType getModificationType(Arguments arguments, Element element, String attributeName, String newAttributeName) {
-        return ModificationType.SUBSTITUTION;
-    }
-
-    @Override
-    protected boolean removeAttributeIfEmpty(Arguments arguments, Element element, String attributeName, String newAttributeName) {
-        return true;
-    }
-
-    @Override
-    protected boolean recomputeProcessorsAfterExecution(Arguments arguments, Element element, String attributeName) {
-        return false;
+        newAttributes.put("action", url);
+        return new BroadleafAttributeModifier(newAttributes, removedAttributes);
     }
 }
